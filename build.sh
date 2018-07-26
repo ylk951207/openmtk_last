a2846471845c91d695384871b3d5b42459a63f53
Park Hyung Eun <hepark@withusp.com>
2018-07-26 06:37:30 +0000

    Modify build.sh, add startup-config,..

--- build.sh
+++ build.sh
@@ -3,11 +3,25 @@
 MTK_IMAGE_NAME=mtk-openwrt-4.0.1.0/bin/targets/mediatek/mt7622-glibc/lede-mediatek-mt7622-MTK-AC2600-RFB1-squashfs-sysupgrade.bin
 VERSION_FILE=mtk-openwrt-4.0.1.0/files/etc/openwrt_release
 
+#Change /tmp directory owner during openwrt compile.
+#'root' user cannot build openwrt, but some packages have to make a directory in /tmp. 
+change_tmp_owner()
+{
+#    echo $1
+    sudo chown $1:$1 /tmp
+}
+
+
 case $1 in
 openwrt)
     echo "Compile openwrt"
+   
+    change_tmp_owner $(whoami)
+
     cd mtk-openwrt-4.0.1.0
     make V=s
+    
+    change_tmp_owner root
     ;;
 bootloader)
     echo "Compile bootloader"
