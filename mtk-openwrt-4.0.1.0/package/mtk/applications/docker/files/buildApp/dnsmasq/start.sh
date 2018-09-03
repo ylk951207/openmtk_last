a66e03171fade57f832078ed0de9c789f7909938
lssnow <lssnow@withusp.com>
2018-09-03 00:49:56 +0000

    Add chrony container but not yet include menuconfig for chrony

--- mtk-openwrt-4.0.1.0/package/mtk/applications/docker/files/buildApp/dnsmasq/start.sh
+++ mtk-openwrt-4.0.1.0/package/mtk/applications/docker/files/buildApp/dnsmasq/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /bin/cp /dnsmasq-* /usr/sbin/dnsmasq
 /dnsmasq.init start
-/usr/sbin/dnsmasq -C /var/etc/dnsmasq.conf.cfg02411c -k -x /var/run/dnsmasq/dnsmasq.cfg02411c.pid
+/usr/sbin/dnsmasq -C /var/etc/dnsmasq.conf.* -k -x /var/run/dnsmasq/dnsmasq.cfg02411c.pid
