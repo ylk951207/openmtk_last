65d115be474844a5732b515f29878ba988c29cae
lssnow <lssnow@withusp.com>
2018-09-17 11:15:08 +0000

    Add script for mini version of net-snmp container image

--- mtk-openwrt-4.0.1.0/package/mtk/applications/docker/files/makeContainer/net-snmp/start.sh
+++ mtk-openwrt-4.0.1.0/package/mtk/applications/docker/files/makeContainer/net-snmp/start.sh
@@ -2,6 +2,6 @@
 /bin/cp -af /net-snmp-*/bin/* /usr/bin
 /bin/cp -af /net-snmp-*/sbin/* /usr/sbin
 /bin/cp -af /net-snmp-*/mibs/* /usr/share/snmp/mibs
-/bin/cp -af /snmpd.conf /etc/config/snmpd
+#/bin/cp -af /snmpd.conf /etc/config/snmpd
 /snmpd.init start
 /usr/sbin/snmpd -c /etc/snmp/snmpd.conf -Lf /dev/null -f
