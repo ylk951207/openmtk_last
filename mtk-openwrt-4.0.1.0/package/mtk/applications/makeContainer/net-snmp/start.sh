652b343ccbea9b9d62276bb2eb0c76176201565c
lssnow <lssnow@withusp.com>
2018-10-22 10:41:14 +0000

    Add SNMP_ARGS for shell environment variable

--- mtk-openwrt-4.0.1.0/package/mtk/applications/makeContainer/net-snmp/start.sh
+++ mtk-openwrt-4.0.1.0/package/mtk/applications/makeContainer/net-snmp/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-/bin/cp -af /net-snmp-*/bin/* /usr/bin
-/bin/cp -af /net-snmp-*/sbin/* /usr/sbin
-/bin/cp -af /net-snmp-*/mibs/* /usr/share/snmp/mibs
+/bin/cp -af /net-snmp-*/bin/* /usr/bin 2>/dev/null
+/bin/cp -af /net-snmp-*/sbin/* /usr/sbin 2>/dev/null
+/bin/cp -af /net-snmp-*/mibs/* /usr/share/snmp/mibs 2>/dev/null
 #/bin/cp -af /snmpd.conf /etc/config/snmpd
 /snmpd.init start
-/usr/sbin/snmpd -c /etc/snmp/snmpd.conf -Lf /dev/null -f
+echo "[start.sh] SNMP_ARGS is $SNMP_ARGS"
+/usr/sbin/snmpd -a 0.0.0.0:$SNMP_ARGS -c /etc/snmp/snmpd.conf -Lf /dev/null -f
